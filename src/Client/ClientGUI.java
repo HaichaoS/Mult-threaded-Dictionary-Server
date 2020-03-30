25a0eabc37f35358734fca69b019efcc0196f691
Haichao Song <haichaos@student.unimelb.edu.au>
2020-03-31 00:32:34 +1100

    add client GUI

---
+++ src/Client/ClientGUI.java
@@ -0,0 +1,27 @@
+package Client;
+
+import javax.swing.*;
+
+/**
+ * Haichao Song
+ * Description:
+ */
+public class ClientGUI {
+
+    private JFrame frame;
+    private Client client;
+
+    public JFrame getFrame() {
+        return frame;
+    }
+
+    public ClientGUI (Client client) {
+        this.client = client;
+        start();
+    }
+
+    public void start() {
+
+    }
+
+}
