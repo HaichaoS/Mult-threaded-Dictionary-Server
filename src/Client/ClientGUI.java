c91851b04497581e63deda285f790368ff172e81
Haichao Song <haichaos@student.unimelb.edu.au>
2020-03-31 16:56:02 +1100

    arrange client GUI

--- src/Client/ClientGUI.java
+++ src/Client/ClientGUI.java
@@ -57,6 +57,55 @@ public class ClientGUI {
             }
         });
 
+        JLabel meaning = new JLabel("The Meaning(s) of the word: ");
+        JScrollPane scrollPane = new JScrollPane();
+        JLabel target = new JLabel("Word:");
+        JTextArea meaningPane = new JTextArea();
+
+        scrollPane.setViewportView(meaningPane);
+        meaningPane.setLineWrap(true);
+        GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+        groupLayout.setHorizontalGroup(
+                groupLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(groupLayout.createSequentialGroup()
+                                .addGap(5)
+                                .addComponent(target, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE))
+                        .addComponent(text, GroupLayout.DEFAULT_SIZE, 450, Short.MAX_VALUE)
+                        .addGroup(groupLayout.createSequentialGroup()
+                                .addGap(5)
+                                .addComponent(meaning, GroupLayout.PREFERRED_SIZE, 244, GroupLayout.PREFERRED_SIZE))
+                        .addGroup(groupLayout.createSequentialGroup()
+                                .addGap(5)
+                                .addComponent(search, GroupLayout.DEFAULT_SIZE, 130, Short.MAX_VALUE)
+                                .addGap(25)
+                                .addComponent(add, GroupLayout.DEFAULT_SIZE, 130, Short.MAX_VALUE)
+                                .addGap(25)
+                                .addComponent(remove, GroupLayout.DEFAULT_SIZE, 130, Short.MAX_VALUE)
+                                .addGap(5))
+                        .addGroup(groupLayout.createSequentialGroup()
+                                .addGap(5)
+                                .addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 439, Short.MAX_VALUE)
+                                .addContainerGap())
+        );
+
+        groupLayout.setVerticalGroup(
+                groupLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(groupLayout.createSequentialGroup()
+                                .addGap(5)
+                                .addComponent(target, GroupLayout.PREFERRED_SIZE, 15, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(text, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
+                                .addGap(5)
+                                .addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE)
+                                .addGap(5)
+                                .addComponent(meaning, GroupLayout.PREFERRED_SIZE, 15, GroupLayout.PREFERRED_SIZE)
+                                .addGap(5)
+                                .addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                        .addComponent(search, GroupLayout.PREFERRED_SIZE, 50, GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(add, GroupLayout.PREFERRED_SIZE, 50, GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(remove, GroupLayout.PREFERRED_SIZE, 50, GroupLayout.PREFERRED_SIZE))
+                                .addGap(8))
+        );
+        frame.getContentPane().setLayout(groupLayout);
     }
 
 }
