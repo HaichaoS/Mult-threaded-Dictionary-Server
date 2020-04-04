89df57406b86217f76d66ca317e36309c82b3a0c
Haichao Song <haichaos@student.unimelb.edu.au>
2020-04-04 17:59:33 +1100

    add execute thread

---
+++ src/Client/ExecuteThread.java
@@ -0,0 +1,65 @@
+package Client;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * Haichao Song
+ * Description:
+ */
+public class ExecuteThread extends Thread  {
+
+    private String address, command, word, meaning;
+    private int port, state;
+
+    public ExecuteThread(String address, int port, String command, String word, String meaning) {
+
+        this.address = address;
+        this.port = port;
+        this.command = command;
+        this.word = word;
+        this.meaning = meaning;
+
+    }
+
+    @Override
+    public void run()  {
+
+        try {
+
+            Socket socket = new Socket(address, port);
+            DataInputStream dis = new DataInputStream(socket.getInputStream());
+            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+
+            JSONObject jsonWrite = new JSONObject();
+            jsonWrite.put("command", command);
+            jsonWrite.put("word", word);
+            jsonWrite.put("meaning", meaning);
+
+            dos.writeUTF(jsonWrite.toJSONString());
+            dos.flush();
+
+            JSONObject jsonRead;
+            JSONParser parser = new JSONParser();
+            jsonRead = (JSONObject) parser.parse(dis.readUTF());
+            meaning = (String) jsonRead.get("meaning");
+
+            dis.close();
+            dos.close();
+            socket.close();
+
+        } catch (IOException) {
+            System.out.println("Error: I/O ERROR!");
+        } catch (ParseException | IOException e) {
+            System.out.println("Error: Parse ERROR!");
+        }
+
+    }
+
+}
