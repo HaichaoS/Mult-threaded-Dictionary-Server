6b0fbf5664b6e94278cecfd046c546a78bab81dd
Haichao Song <haichaos@student.unimelb.edu.au>
2020-04-14 14:56:32 +1000

    add dict comment

--- src/Server/Dict.java
+++ src/Server/Dict.java
@@ -13,7 +13,9 @@ import java.util.ArrayList;
 
 /**
  * Haichao Song
- * Description:
+ * Description: the class that is responsible for:
+ * 1) read the dictionary at the start of the server and store it into JSON array form.
+ * 2) Do operation according to the command on JSON array and JSON dictionary.
  */
 public class Dict {
 
@@ -42,10 +44,12 @@ public class Dict {
         }
     }
 
+    /* Check if the word is already exist in the dictionary */
     public boolean isWordExist(String word) {
         return dict.toString().contains("\"Word\":\""+word+"\"");
     }
 
+    /* Get the word information of a word */
     public ArrayList<String> searchDict(String word) {
 
         ArrayList<String> result = new ArrayList<>();
@@ -68,6 +72,7 @@ public class Dict {
         }
     }
 
+    /* Add the word and its information into dictionary */
     public boolean addDict(String word, String meaning, String synonym) {
         if (isWordExist(word)) {
             System.out.println("Server check");
@@ -92,6 +97,7 @@ public class Dict {
         }
     }
 
+    /* Remove the word and its information in the dictionary */
     public boolean removeDict(String word) {
         if (isWordExist(word)) {
             for (int i = 0 ; i < dict.size(); i ++) {
